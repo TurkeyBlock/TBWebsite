73d35225f36fd499af4e40740462bc153298463e
TurkeyBlock <78536986+TurkeyBlock@users.noreply.github.com>
2025-06-16 09:27:13 -0500

    sync & edge call

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 "use client"
 import TextInput from "./_components/textInput/page"
 import { useState} from "react";
+import { supabase } from '../lib/supabase'
 
+async function getTable(){
+  const { data, error } = await supabase.functions.invoke('postgres-edge', {
+    body: { name: 'Functions' },
+  })
+  console.log(data);
+}
 export default function Page() {
   const [inputText, setInputText] = useState("");
   return (
@@ -10,7 +17,7 @@ export default function Page() {
       <h1>Hello D&D Nerds</h1>
       <h2>@Turkeyblock.org</h2>
       <a>aa</a>
-      
+      <button style={{padding:"3px"}} onClick={getTable}>"Click for Table"</button>
       And this is text 1
       And this is text 2
       Etc Etc
