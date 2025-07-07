46e72469c99c6202ee52948d7ccb01b978f8bc61
TurkeyBlock <78536986+TurkeyBlock@users.noreply.github.com>
2025-07-07 14:35:00 -0500

    leaving work soon

--- app/protected/games/page.tsx
+++ app/protected/games/page.tsx
@@ -3,25 +3,57 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 
+//To-do: Timed make-moves w/ respective functions.
 const Games = () => {
-    const board = new Array(9).fill(null);
+    const TicTacToe = [null,null,'O',null,'X','O','X',null,null];
+    //new Array(9).fill(null);
+    const ConnectFour = new Array(7).fill().map(() => Array(6).fill(null));
     return(
         <div className={styles.container}>
-        <Link href="./games/tic-tac-toe" className={styles.link}>
-            <div className = {styles.board}>
-                {/*--------------------*/}
-                {board.map((cell, index) => (
-                    <div
-                    key={index}
-                    className={styles.cell}
-                    >
-                    {cell}
+            <Link href="./games/tic-tac-toe" className={[styles.link, styles.container].join(" ")}>
+                <div className={styles.subContainer}>
+                    <div className = {styles.TicTacToe}>
+                        {/*--------------------*/}
+                        {TicTacToe.map((cell, index) => (
+                            <div
+                            key={index}
+                            className={styles.cell}
+                            >
+                            {cell}
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-            Play TicTacToe
-        </Link>
-        <Link href="./games/connect-four" className={styles.link}>Play Connect-Four</Link>
+                </div>
+                <div className={styles.subContainer}>
+                    Play TicTacToe
+                </div>
+            </Link>
+            <Link href="./games/connect-four" className={[styles.link, styles.container].join(" ")}>
+                <div className = {styles.subContainer}>
+                    <div className = {styles.ConnectFour}>
+                        {ConnectFour.map((col, colIndex) => (
+                            <div
+                                key={colIndex}
+                                className = {styles.column}
+                            >
+                            {col.map((slot, slotIndex) => (
+                                <div
+                                    key={slotIndex}
+                                    className = {ConnectFour[colIndex][slotIndex] == null ? styles.slot
+                                    : (ConnectFour[colIndex][cellIndex] == 'X') ? [styles.slot, styles.tokenA].join(" ")
+                                    : [styles.slot, styles.tokenB].join(" ")}
+                                >
+                                {slot}
+                                </div>
+                            ))}
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className = {styles.subContainer}>
+                    Play Connect-Four
+                </div>
+            </Link>
         </div>
 
 
