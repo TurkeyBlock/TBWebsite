e01fee2e507092e76129f7bc75f27d1835d1a1a5
TurkeyBlock <78536986+TurkeyBlock@users.noreply.github.com>
2025-09-08 14:55:10 -0500

    named the waves page

--- app/protected/games/offline/page.js
+++ app/protected/games/offline/page.js
@@ -1,6 +1,6 @@
 "use client"
 import styles from "./page.module.css"
-export default function () {
+export default function Waves () {
     return(
         <div className = {styles.restrictedBox}>
             <div className = {styles.waterContainer}>
